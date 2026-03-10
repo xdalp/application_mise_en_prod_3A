50bbc17e02df8e1007f116abeb46337be9e1ab46
xdalp <mgarbe744@gmail.com>
2026-03-10 09:31:56 +0000

    Modif nom api

--- app/api.py
+++ app/api.py
@@ -10,7 +10,7 @@ model = sio.load("model.skops", trusted=unknown_types)
 app = FastAPI(
     title="Démonstration du modèle de prédiction de survie sur le Titanic",
     description=
-    "<b>Application de prédiction de survie sur le Titanic</b> 🚢 <br>Une version par API pour faciliter la réutilisation du modèle 🚀" +\
+    "<b>TADAM !!!! Application de prédiction de survie sur le Titanic</b> 🚢 <br>Une version par API pour faciliter la réutilisation du modèle 🚀" +\
         "<br><br><img src=\"https://media.vogue.fr/photos/5faac06d39c5194ff9752ec9/1:1/w_2404,h_2404,c_limit/076_CHL_126884.jpg\" width=\"200\">"
     )
 
